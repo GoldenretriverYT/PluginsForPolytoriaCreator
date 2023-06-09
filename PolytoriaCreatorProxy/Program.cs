3b0c49d64487db6cce03c80754145c0ce181b0a1
GoldenretriverYT <49924528+GoldenretriverYT@users.noreply.github.com>
2023-06-09 09:14:58 +0200

    Add information about the proxy app

--- PolytoriaCreatorProxy/Program.cs
+++ PolytoriaCreatorProxy/Program.cs
@@ -2,6 +2,14 @@
 using System.Reflection;
 
 namespace PolytoriaCreatorProxy {
+    /// <summary>
+    /// Looking at this code, you may wonder why this is needed. And I can tell you why! well roughly at least.
+    /// When the Polytoria Creator gets launched from the browser, the wrong working directory is
+    /// set (to 'C:\Windows\System32') - this messes up MelonLoader and therefore we have to make
+    /// a small proxy app that starts the Polytoria Creator using the correct working directory.
+    /// 
+    /// thanks for listening to my ted talk.
+    /// </summary>
     internal class Program {
         static void Main(string[] args) {
             string dir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
